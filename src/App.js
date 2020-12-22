f035227af85b796480d294db6b32b9c6ba1f6600
Okraku Isaac Antwi <antwisaac7@gmail.com>
2020-12-22 02:09:19 +0000

    Reading List Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import Navbar from "./components/Navbar";
 import BookContextProvider from "./contexts/BookContext";
+import BookList from "./components/BookList";
+import NewBookForm from "./components/BookForm";
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
+        <NewBookForm />
       </BookContextProvider>
     </div>
   );
